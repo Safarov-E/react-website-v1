ded594cc0d690b0ae87e299109fed4082c3d7cd5
Safarov <elkhan.safarov10@yandex.com>
2020-10-28 22:04:09 +0500

    Маршрутизация

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import Navbar from './component/navbar'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Yo</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
