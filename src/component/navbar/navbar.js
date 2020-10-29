fb97b9841a769a1c75d6c1a7247f4eec7ddaf1f5
Safarov <elkhan.safarov10@yandex.com>
2020-10-29 22:41:05 +0500

    Событие изменении размера документа

--- src/component/navbar/navbar.js
+++ src/component/navbar/navbar.js
@@ -1,10 +1,21 @@
 import React, {useState} from 'react'
+import Button from '../button'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     const [click, setClick] = useState(false)
+    const [button, setButton] = useState(true)
+
     const handleClick = () => setClick(!click)
     const closeMobileMenu = () => setClick(false)
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    }
+    window.addEventListener('resize', showButton)
     return (
         <>
             <nav className="navbar">
@@ -37,6 +48,7 @@ const Navbar = () => {
                             </Link>
                         </li>
                     </ul>
+                    {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
                 </div>
             </nav>
         </>
