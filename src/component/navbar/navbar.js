59723e685135753d1384c8c6c84776c6243c01af
Safarov <elkhan.safarov10@yandex.com>
2020-10-29 23:08:11 +0500

    Обработчик скрытие элемента

--- src/component/navbar/navbar.js
+++ src/component/navbar/navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Button from '../button'
 import { Link } from 'react-router-dom'
 import './navbar.css'
@@ -16,12 +16,15 @@ const Navbar = () => {
             setButton(true)
         }
     }
+    useEffect(() => {
+        showButton()
+    }, [])
     window.addEventListener('resize', showButton)
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
                         TRVL<i className="fab fa-typo3"></i>
                     </Link>
                     <div className="menu-icon" onClick={handleClick}>
