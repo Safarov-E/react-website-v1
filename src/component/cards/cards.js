d1276c94ac0ada6b76c2b50455ae49e9e7795eef
Safarov <elkhan.safarov10@yandex.com>
2020-11-01 15:28:54 +0500

    Передача пропс в компонент CardItem

--- src/component/cards/cards.js
+++ src/component/cards/cards.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import CardItem from '../card-item'
 import './cards.css'
+import ImgNine from '../../assets/images/img-9.jpg'
+import ImgTwo from '../../assets/images/img-2.jpg'
+import ImgThree from '../../assets/images/img-3.jpg'
+import ImgFour from '../../assets/images/img-4.jpg'
+import ImgEight from '../../assets/images/img-8.jpg'
 
 function Cards() {
     return (
@@ -9,7 +14,38 @@ function Cards() {
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                        <CardItem />
+                        <CardItem 
+                            src={ImgNine}
+                            text="Explore the hidden waterfall deep inside the Amazon Jungle"
+                            label="Adventure"
+                            path="/services"
+                        />
+                        <CardItem 
+                            src={ImgTwo}
+                            text="Travel through the Islands of Bali in a Private Croise"
+                            label="Luxury"
+                            path="/services"
+                        />
+                    </ul>
+                    <ul className="cards__items">
+                        <CardItem 
+                            src={ImgThree}
+                            text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
+                            label="Mystery"
+                            path="/services"
+                        />
+                        <CardItem 
+                            src={ImgFour}
+                            text="Experience Football on Top of the Himilayan Mountains"
+                            label="Adventure"
+                            path="/products"
+                        />
+                        <CardItem 
+                            src={ImgEight}
+                            text="Ride through the Sahara Desert on a guided camel tour"
+                            label="Adventure"
+                            path="/sign-up"
+                        />
                     </ul>
                 </div>
             </div>
